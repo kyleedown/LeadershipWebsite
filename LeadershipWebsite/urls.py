732e467032956b234659bdeec5032ada3977c484
Kylee Down <3095263d@student.gla.ac.uk>
2026-04-02 11:20:27 -0700

    LSW-8: Create base HTML template and site navigation

--- LeadershipWebsite/urls.py
+++ LeadershipWebsite/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from content import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("admin/", admin.site.urls),
     path("articles/", include("content.urls")),
     path("quizzes/", include("quizzes.urls")),
