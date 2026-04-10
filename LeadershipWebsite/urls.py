768762d9448e6fd566e4f693a9bdb65fd52cd015
Kylee Down <3095263d@student.gla.ac.uk>
2026-04-10 13:46:23 -0700

    Article with Image

--- LeadershipWebsite/urls.py
+++ LeadershipWebsite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from content import views
 
 urlpatterns = [
@@ -25,4 +27,4 @@ urlpatterns = [
     path("articles/", include("content.urls")),
     path("quizzes/", include("quizzes.urls")),
     path('summernote/', include('django_summernote.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- content/admin.py
+++ content/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Article
-from django_summernote.admin import SummernoteModelAdmin 
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Article)
 
 class ArticleAdmin(SummernoteModelAdmin):
     summernote_fields = ('body',)
+
 
 admin.site.register(Article, ArticleAdmin)
